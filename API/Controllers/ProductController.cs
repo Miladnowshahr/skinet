249c0f011c0248e535e19578f82955093debca6d
Milad Nowshahr <miladnowshahr@yahoo.com>
2024-09-12 17:17:20 +0330

    Add project files.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id  !ProductExist(id)) return BadRequest("Cannot update this product");
+
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            if (!ProductExist(id)) return NotFound();
+            _context.Products.Remove(new Product { Id = id });
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProductExist(int id)
+        {
+            return _context.Products.Any(x=>x.Id == id);
+        }
+
+
+    }
+}
